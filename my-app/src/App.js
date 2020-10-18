1126284d82a36b1635147425c7a570a236b8e013
L0u1ss <48998681+L0u1ss@users.noreply.github.com>
2020-10-18 02:59:12 -0300

    creat home
    app.js passa ah ser apenas redirecionador, e passo tudo para o home.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,51 +1,56 @@
 import React from 'react';
-import { Button, Grid, List, ListItem, ListItemText, Divider } from '@material-ui/core';
-import Listagem from './components/Listagem';
+import AddPessoa from './components/AddPessoa';
+import Home from './components/Home';
+import EditPessoa from './components/EditPessoa'
+import VerPessoa from './components/VerPessoa'
+import AddContato from './components/AddContato';
+import EditContato from './components/EditContato'
 
-function App() {
-    return (
-        <>
-            <div>
-                <form>
-                    <Grid container direction="row" alignItems={"center"} spacing={1} xs={12}>
-                        <Grid item xs={3}>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <h1>Lista de Contatos</h1>
-                        </Grid>
-                        <Grid item xs={4}>
-                            <Button variant="contained" size="large" color="primary">
-                                Adcicionar Contato &nbsp;
-                            </Button>
-                        </Grid>
-                    </Grid>
-                </form>
-            </div>
-            <div>
-                <form>
-                <Grid container direction="collumn" spacing={1}>
-                        <Grid item xs={12}>
-                            <List component="nav" aria-label="main mailbox folders">
-                                <ListItem>
-                                    <ListItemText primary="Nome/Sobrenome" />
-                                    <ListItemText primary="Celular" />
-                                    <ListItemText primary="Telefone" />
-                                    <ListItemText primary="E-mail" />
-                                    <ListItemText primary="Outros" />
-                                    <ListItemText primary="Opções" />
-                                </ListItem>
-                                <Divider />
-                                
-                                <Listagem />
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { page: "home", user: { id: 0, nome: "", sobrenome: "" }, contato: { id: 0, contato: "", tipo: 0 } };
+    }
 
-                            </List>
-                        </Grid>
-                    </Grid>
-                </form>
-            </div>
-        </>
-    );
+    changePage(page) {
+        this.setState({ page: page })
+    }
+
+    setUser(user) {
+        this.setState({ user: user })
+    }
+
+    setContato(contato) {
+        this.setState({ contato: contato })
+    }
+
+    render() {
+        if (this.state.page === "home") {
+            return (
+                <Home changePage={(page) => this.changePage(page)} setUser={(user) => this.setUser(user)} />
+            );
+        } else if (this.state.page === "addPessoa") {
+            return (
+                <AddPessoa changePage={(page) => this.changePage(page)} />
+            );
+        } else if (this.state.page === "editPessoa") {
+            return (
+                <EditPessoa userid={this.state.user.id} nome={this.state.user.nome} sobrenome={this.state.user.sobrenome} changePage={(page) => this.changePage(page)} />
+            );
+        } else if (this.state.page === "verPessoa") {
+            return (
+                <VerPessoa userid={this.state.user.id} nome={this.state.user.nome} sobrenome={this.state.user.sobrenome} changePage={(page) => this.changePage(page)} setContato={(contato) => this.setContato(contato)} opcoes={{1:"Telefone",2:"Celular",3:"E-mail",4:"Outros"}} />
+            );
+        } else if (this.state.page === "addContato") {
+            return (
+                <AddContato userid={this.state.user.id} nome={this.state.user.nome} sobrenome={this.state.user.sobrenome} changePage={(page) => this.changePage(page)} />
+            );
+        } else if (this.state.page === "editContato") {
+            return (
+                <EditContato userid={this.state.user.id} nome={this.state.user.nome} sobrenome={this.state.user.sobrenome} contatoid={this.state.contato.id} contato={this.state.contato.contato} tipo={this.state.contato.tipo} changePage={(page) => this.changePage(page)} />
+            );
+        }
+    }
 }
 
 export default App;
-
---
+++ my-app/src/components/Home.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Grid, List, ListItem, ListItemText, Button, Divider } from '@material-ui/core';
+import Listagem from './Listagem';
+import axios from 'axios'
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { pessoas: [] };
+    }
+
+    listarPessoas() {
+        axios.get('http://localhost/Prova-dev-CBMSE/api/pessoas/listar.php')
+            .then(response => {
+                this.setState({ pessoas: response.data })
+            })
+    }
+
+    componentDidMount() {
+        this.listarPessoas()
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div>
+                    <form>
+                        <Grid container direction="row" alignItems={"center"} spacing={1}>
+                            <Grid item xs={3}>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <h1>Lista de Contatos</h1>
+                            </Grid>
+                            <Grid item xs={4}>
+                                <Button variant="contained" size="large" color="primary" onClick={() => this.props.changePage("addPessoa")}>
+                                    Adcicionar Pessoa &nbsp;
+                                    </Button>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </div>
+                <div>
+                    <form>
+                        <Grid container direction="column" spacing={1}>
+                            <Grid item xs={5}>
+                                <List component="nav" aria-label="main mailbox folders">
+                                    <ListItem>
+                                        <ListItemText primary="Nome Completo" />
+                                        <ListItemText primary="Opções" />
+                                    </ListItem>
+                                    <Divider />
+                                </List>
+                            </Grid>
+                            <Grid item xs={12}>
+                                {this.state.pessoas.map(el => (
+                                    <Listagem nome={el.nome} sobrenome={el.sobrenome} id={el.id} listarPessoas={() => this.listarPessoas()} setUser={(id) => this.props.setUser(id)} changePage={(page) => this.props.changePage(page)} />
+                                ))}
+                            </Grid>
+                        </Grid>
+                    </form>
+                </div>
+            </ React.Fragment>
+        );
+    }
+}
+
+export default Home;
+
